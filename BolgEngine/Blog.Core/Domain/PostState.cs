d559aa0d1862d977d1ede1732bf1d53dd7386909
Emilio Morbidoni <emilio.morbidoni@urbetrack.com>
2023-05-29 00:23:49 -0300

    removes unnesesary folders

--- BolgEngine/Blog.Core/Domain/PostState.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Blog.Core.Domain
-{
-    public enum PostState : short
-    {
-        Pending = 0,
-        Aproved = 1,
-        Rejected = 2
-    }
-}
--- BolgEngine/Blog.Core/Features/GetPendingPosts/GetPendingPostsRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Blog.Core.Features.GetPendingPosts
-{
-    using System;
-    using MediatR;
-
-    public class GetPendingPostsRequest : IRequest<GetPendingPostsResponse>
-    {        
-    }
-}
